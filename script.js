24f015567b70ea9bad29fa002cc1ddc9651d1c73
LuigiEBO <luigigames2903@gmail.com>
2024-01-16 12:21:32 -0300

    main

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ function togglemode() {
   const img = document.querySelector("#profile img")
   ///vc esqueceu de por a letra maiuscula (l) por isso tava dando erro
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/foto.jpeg")
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/foto.jpeg")
   }
   //pegar a tag img
   //substituir a imagem 
   // se tiver light mode adicionar a imagem light
   // se tiver sem light mode manter a imagem normal
    if (html.classList.contains("light")) {
-     img.setAttribute("alt", "Foto mayk brito com oculos escuro e camisa azul")
+     img.setAttribute("alt", "Foto do Luigi com camisa Preta")
    } else {
-     img.setAttribute("alt", "Foto do mayk brito com camisa Preta")
+     img.setAttribute("alt", "Foto do Luigi com camisa Preta")
    }
 }
